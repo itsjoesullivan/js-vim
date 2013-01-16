b966c4f0c4fb67ccb88f1f26e9d67bb1d453cf3c
Joe Sullivan <joe@clearspring.com>
2013-01-15 20:33:23 -0500

    format

--- vim/plugins-core/visual.js
+++ vim/plugins-core/visual.js
@@ -1,30 +1,30 @@
 (function() {
 	var start,
-		end;
+	end;
 	var visual = {
 		idle: {
 			'v': function() {
-				vim.set('mode','visual');
+				vim.set('mode', 'visual');
 			}
 		},
 		on: {
 			'visual': function() {
 				vim.notify('-- VISUAL --');
-				start = [cursor.line,cursor.char];
+				start = [cursor.line, cursor.char];
 				console.log(start);
-				end = [cursor.line,cursor.char];
+				end = [cursor.line, cursor.char];
 			},
 			'visual-resume': function() {
-				end = [cursor.line,cursor.char];
+				end = [cursor.line, cursor.char];
 			}
 		},
 		visual: {
 			'\[hjkl\]': function(key) {
 				console.log(key);
-				vim.exec(['escape',key]);
-				vim.set('mode','visual-resume');
-				console.log([start,end]);
-				cursor.highlight([start,end]);
+				vim.exec(['escape', key]);
+				vim.set('mode', 'visual-resume');
+				console.log([start, end]);
+				cursor.highlight([start, end]);
 			}
 		}
 	};
