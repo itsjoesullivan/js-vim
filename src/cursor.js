09bdc1922210c31bfa9ce9c6ec6d063b21a06be5
Joe Sullivan <joe@clearspring.com>
2013-01-23 21:46:55 -0500

    refactor

--- src/cursor.js
+++ src/cursor.js
@@ -1,114 +1,116 @@
 var Cursor = Backbone.Model.extend({
-    line:0,
-    char:0,
-    position: {
-        line:0,
-        char:0
-    },
-    initialize: function() {
-        this.set('view',new CursorV({model:this}));
-        this.on('change:line', function(m,v,o) {
-            this.position.line = this.line = v;
-            this.get('view').render();
-        },this);
-        this.on('change:char', function(m,v,o) {
-            this.position.char = this.char = v;
-            this.get('view').render();
-        },this);
-        
-    },
-    nextLine: function() {
-	    var lines = doc.get('lines').length;
-	    if(lines-1 > cursor.line) {
-	return	   cursor.set('line',cursor.line+1);
-	    }
-	    if(lines > 1) {
-		return    cursor.set('line',0);
-		}
-    },
-		   
-    /* Expect
-     * 	[ [ startLine, startChar], [ endLine, endChar ] ]
-     */	
-    highlight: function(range) {
-	    if(vim.get('selection')) {
-		vim.get('selection').forEach(function(char) {
-			$(char.get('el')).removeClass('highlight');
-			$(char.get('el')).removeClass('last');
-			$(char.get('el')).removeClass('first');
-		});
-	    }
-	var startLine = range[0][0],
-	    	startChar = range[0][1],
-		endLine = range[1][0],
-		endChar = range[1][1];
-	var curLine = startLine;
-	var chars = doc.get('lines').at(startLine).get('chars').rest(startChar);
-	if(endLine === startLine) {
-		chars = chars.slice(0,endChar-startChar+1);
-	} else {
-		while(++curLine < endLine) {
-			chars = chars.concat(doc.get('lines').at(curLine).get('chars').toArray());
-		}
-		chars = chars.concat(doc.get('lines').at(curLine).get('chars').first(endChar));
-	}
-	vim.set('selection',chars);
-	chars.forEach(function(char) {
-		$(char.get('el')).addClass('highlight');
-	})
-	$(_(chars).first().get('el')).addClass('first');
-	$(_(chars).last().get('el')).addClass('last');
+	line: 0,
+	char: 0,
+	position: {
+		line: 0,
+		char: 0
+	},
+	initialize: function() {
+		this.set('view', new CursorV({
+			model: this
+		}));
+		this.on('change:line', function(m, v, o) {
+			this.position.line = this.line = v;
+			this.get('view').render();
+		}, this);
+		this.on('change:char', function(m, v, o) {
+			this.position.char = this.char = v;
+			this.get('view').render();
+		}, this);
 
-    }
+	},
+	nextLine: function() {
+		var lines = doc.get('lines').length;
+		if (lines - 1 > cursor.line) {
+			return cursor.set('line', cursor.line + 1);
+		}
+		if (lines > 1) {
+			return cursor.set('line', 0);
+		}
+	},
+
+	/* Expect
+	 * 	[ [ startLine, startChar], [ endLine, endChar ] ]
+	 */
+	highlight: function(range) {
+		vim.set('range',range);
+		if (vim.get('selection')) {
+			vim.get('selection').forEach(function(char) {
+				$(char.get('el')).removeClass('highlight');
+				$(char.get('el')).removeClass('last');
+				$(char.get('el')).removeClass('first');
+			});
+		}
+		var startLine = range[0][0],
+			startChar = range[0][1],
+			endLine = range[1][0],
+			endChar = range[1][1];
+		var curLine = startLine;
+		var chars = doc.get('lines').at(startLine).get('chars').rest(startChar);
+		if (endLine === startLine) {
+			chars = chars.slice(0, endChar - startChar + 1);
+		} else {
+			while (++curLine < endLine) {
+				chars = chars.concat(doc.get('lines').at(curLine).get('chars').toArray());
+			}
+			chars = chars.concat(doc.get('lines').at(curLine).get('chars').first(endChar));
+		}
+		vim.set('selection', chars);
+		chars.forEach(function(char) {
+			$(char.get('el')).addClass('highlight');
+		})
+		$(_(chars).first().get('el')).addClass('first');
+		$(_(chars).last().get('el')).addClass('last');
+
+	}
 });
 
 var CursorV = Backbone.View.extend({
-    el: json2html({'pre.cursor':' '}),
-    render: function() {
-        console.log('render');
-        var position = this.model.position;
-        if(position.line < 0) {
-            this.model.set('line',0);
-            return this.render();
-        }
-        if(position.char < 0) {
-            this.model.set('char',0);
-            return this.render();
-        }
-        //move the cursor to the position that you're in by inserting it after the previous character on that line.
-        if(!vim.get('doc').get('lines').length) {
-            vim.set('mode','idle');
-            return;
-        }
-        var line = doc.get('lines').at(position.line);
-        
-        if(line.get('chars').length < position.char) {
-            this.model.set('char',line.get('chars').length);
-        }
-        if(position.char === 0 || !line.get('chars').length) { //add to the line's el if it is empty
-            $(line.get('view').el).append(this.el);
-        } else { //otherwise find the last char and 
-		console.log(line);
-		console.log(position);
-            $(line.get('chars').at(position.char === 0 ? 0 : position.char-1).get('el')).after(this.el);
-        }
-      
-        //we fuck up the character we're on top of to keep the cursor relatively positioned. So, if we did, put it back.
-        if(this.fixThisOne) {
-            this.fixThisOne.style.display = 'inline';
-        }
-        
-        //check if we're on top of a character. grab its value and display that, then hide it under fixThisOne, and switch the cursor colors.
-        var onTopOf = doc.get('lines').at(position.line).get('chars').at(position.char);
-        if(onTopOf) {
-            this.el.style.color = '#eee';
-            this.el.innerHTML = onTopOf.get('val');
-            this.fixThisOne = onTopOf.get('el');
-            this.fixThisOne.style.display = 'none';
-        } else {
-            this.el.style.color = '#eee';
-            this.el.innerHTML = ' ';
-        }
-        this.el.scrollIntoViewIfNeeded();
-    }
+	el: json2html({
+		'pre.cursor': ' '
+	}),
+	render: function() {
+		var position = this.model.position;
+		if (position.line < 0) {
+			this.model.set('line', 0);
+			return this.render();
+		}
+		if (position.char < 0) {
+			this.model.set('char', 0);
+			return this.render();
+		}
+		//move the cursor to the position that you're in by inserting it after the previous character on that line.
+		if (!vim.get('doc').get('lines').length) {
+			vim.set('mode', 'idle');
+			return;
+		}
+		var line = doc.get('lines').at(position.line);
+
+		if (line.get('chars').length < position.char) {
+			this.model.set('char', line.get('chars').length);
+		}
+		if (position.char === 0 || !line.get('chars').length) { //add to the line's el if it is empty
+			$(line.get('view').el).append(this.el);
+		} else { //otherwise find the last char and 
+			$(line.get('chars').at(position.char === 0 ? 0 : position.char - 1).get('el')).after(this.el);
+		}
+
+		//we fuck up the character we're on top of to keep the cursor relatively positioned. So, if we did, put it back.
+		if (this.fixThisOne) {
+			this.fixThisOne.style.display = 'inline';
+		}
+
+		//check if we're on top of a character. grab its value and display that, then hide it under fixThisOne, and switch the cursor colors.
+		var onTopOf = doc.get('lines').at(position.line).get('chars').at(position.char);
+		if (onTopOf) {
+			this.el.style.color = '#eee';
+			this.el.innerHTML = onTopOf.get('val');
+			this.fixThisOne = onTopOf.get('el');
+			this.fixThisOne.style.display = 'none';
+		} else {
+			this.el.style.color = '#eee';
+			this.el.innerHTML = ' ';
+		}
+		this.el.scrollIntoViewIfNeeded();
+	}
 });
--- src/doc.js
+++ src/doc.js
@@ -1,214 +1,241 @@
 var Doc = Backbone.Model.extend({
-    initialize: function(attrs) {
-        this.set('lines',new LineC() );
-        var view = new DocV({model:this});
-        this.set('view',view);
-        if(typeof attrs === 'object') {
-            if('text' in attrs) {
-                this.setLines(attrs.text);
-            }
-        }
-        if(vim.get('mode') === 'insert' && !this.get('lines').length) {
-            var line = new Line();
-            this.get('lines').add(line);
-            line.get('view').render();
-        }
-        vim.on('change:mode', function(m,mode,options) {
-            switch(mode) {
-                case 'insert':
-                    if(!this.get('lines').length) {
-                        this.get('lines').add({val:''});
-                    }
-                    break;
-                default:
-                    break;
-            }
-        },this);
-    },
-    at: function(position) {
-        if(typeof position === 'number') {
-            return this.get('lines').at(position);
-        }
-        var line = cursor.line, char = -1;
-        if(typeof position === 'object') {
-            if(position.length === 2) {
-                line = position[0];
-                char = position[1];
-            }
-            if('line' in position) {
-                line = position.line;
-            }
-            if('char' in position) {
-                char = position.char;
-            }
-        }
-        line = this.get('lines').at(line)
-        return char === -1 ? line : line.get('chars').at(char);
-    },
-    newLine: function() {
-        var curLine = this.get('lines').at(cursor.line),
-            curChars = curLine.get('chars'),
-            curText = curLine.toText(),
-            remainder = curText.substring(cursor.char);
-        curChars.rest(cursor.char).forEach(function(char) {
-            char.delete();
-        });
-        this.get('lines').add(new Line({val:remainder}),{at:cursor.line+1});
-        this.get('lines').rest(cursor.line+1).forEach(function(line) {
-            line.get('view').render();
-        });
-        vim.exec(['escape','j','0','i']);
-        
-        
-        /*vim.get('cursor').set({
+	initialize: function(attrs) {
+		this.set('lines', new LineC());
+		var view = new DocV({
+			model: this
+		});
+		this.set('view', view);
+		if (typeof attrs === 'object') {
+			if ('text' in attrs) {
+				this.setLines(attrs.text);
+			}
+		}
+		if (vim.get('mode') === 'insert' && !this.get('lines').length) {
+			var line = new Line();
+			this.get('lines').add(line);
+			line.get('view').render();
+		}
+		vim.on('change:mode', function(m, mode, options) {
+			switch (mode) {
+				case 'insert':
+					if (!this.get('lines').length) {
+						this.get('lines').add({
+							val: ''
+						});
+					}
+					break;
+				default:
+					break;
+			}
+		}, this);
+	},
+	at: function(position) {
+		if (typeof position === 'number') {
+			return this.get('lines').at(position);
+		}
+		var line = cursor.line,
+			char = -1;
+		if (typeof position === 'object') {
+			if (position.length === 2) {
+				line = position[0];
+				char = position[1];
+			}
+			if ('line' in position) {
+				line = position.line;
+			}
+			if ('char' in position) {
+				char = position.char;
+			}
+		}
+		line = this.get('lines').at(line)
+		return char === -1 ? line : line.get('chars').at(char);
+	},
+	newLine: function() {
+		var curLine = this.get('lines').at(cursor.line),
+			curChars = curLine.get('chars'),
+			curText = curLine.toText(),
+			remainder = curText.substring(cursor.char);
+		curChars.rest(cursor.char).forEach(function(char) {
+			char.delete();
+		});
+		this.get('lines').add(new Line({
+			val: remainder
+		}), {
+			at: cursor.line + 1
+		});
+		this.get('lines').rest(cursor.line + 1).forEach(function(line) {
+			line.get('view').render();
+		});
+		vim.exec(['escape', 'j', '0', 'i']);
+
+
+		/*vim.get('cursor').set({
             'char':0,
             'line':vim.get('cursor').line+1
         });*/
-        
-    },
-    add: function(key,keyCode) {
-        switch(keyCode) {
-            /*case 13: // '\r'
-                console.log('its a new line')
+
+	},
+	add: function(key, keyCode) {
+		switch (keyCode) {
+			/*case 13: // '\r'
                 this.newLine();
                 break;*/
-            default:
-                switch(key) {
-                    
-                    case '\r':
-                        this.newLine();
-                        break;
-                    default:
-                        this.get('lines').at(vim.get('cursor').line).get('chars').add({val:key},{at:cursor.char});
-                        vim.get('cursor').set('char',cursor.char+1);
-                        break;
-                }
-                break;
-                
-        }
-    },
-    setLines: function(text) {
-        var lines = text.split('\n');
-        _(lines).forEach(function(line) {
-            this.get('lines').add({val:line});
-        },this);
-    },
-    toText: function() {
-        var lines = [];
-        this.get('lines').forEach(function(line) {
-            lines.push(line.toText());
-        });
-        return lines.join('\n');
-    },
-    readFile: function() {
-        log('readFile');
-        vim.set('doc',this);
-        if(!this.get('file') && !this.get('appFile')) {
-            throw 'No file';
-        }
-        var that = this;
-        if(this.get('appFile')) {
-            var appFile = this.get('appFile');
-            appFile.file(function(file) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    log('reader.onload');
-                    that.set('text',e.target.result);
-                    that.setLines(that.get('text'));
-                    vim.open(that);
-                };
-                reader.onerror = function(e) { 
-                    throw e;
-                };
-                log('reader.readAsText');
-                reader.readAsText(file);
-            }, function(e) { throw e; });
-        }
-        
-        var reader = new FileReader();
-        reader.onload = function() {
-            log('reader.onload');
-            that.set('text',reader.result);
-            that.setLines(that.get('text'));
-            vim.open(that);
-        };
-        log('reader.readAsText');
-        reader.readAsText(this.get('file'));
-    },
-    save: function() {
-        log('doc.save');
-        if(this.get('file') || this.get('appFile')) {
-            this.saveFile();
-        }
-    },
-    saveFile: function() {
-        if(!this.get('file') && !this.get('appFile')) {
-            throw "No file";
-        }
-        var that = this;
-        if(this.get('file')) {
-            var file = this.get('file');
-            file.createWriter(function(writer) {
-                writer.onwriteend = function(e) {
-                    that.trigger('saved');
-                };
-                writer.onerror = function(e) {
-                    throw e;
-                }
-                writer.write(new Blob[that.toText()], {type: 'text/plain'});
-            }, function(e) { console.log(e); });
-        } else if(this.get('appFile')) {
-            var file = this.get('appFile');
-            file.createWriter(function(writer) {
-                log('file.createWriter');
-                    writer.onerror = function(e) {
-                        log('writer.onerror');
-                        throw e;
-                    };
-                    var blob = new Blob([that.toText()], {type: 'text/plain'});
-                    writer.truncate(blob.size);
-                    
-                    //truncating is a write
-                    writer.onwriteend = function() {
-                        log('writer.truncate');
-                        
-                        //then you actually write
-                        writer.write(blob);
-                        writer.onwriteend = function() {
-                            log('writer.writeend');
-                            that.trigger('ready');
-                        }
-                    }
-                    
-            }, function(e) { throw e; });
-        }
-        
-    }
+			default: switch (key) {
+
+				case '\r':
+					this.newLine();
+					break;
+				default:
+					this.get('lines').at(vim.get('cursor').line).get('chars').add({
+						val: key
+					}, {
+						at: cursor.char
+					});
+					vim.get('cursor').set('char', cursor.char + 1);
+					break;
+			}
+			break;
+
+		}
+	},
+	setLines: function(text) {
+		var lines = text.split('\n');
+		_(lines).forEach(function(line) {
+			this.get('lines').add({
+				val: line
+			});
+		}, this);
+	},
+	toText: function() {
+		var lines = [];
+		this.get('lines').forEach(function(line) {
+			lines.push(line.toText());
+		});
+		return lines.join('\n');
+	},
+	readFile: function() {
+		log('readFile');
+		vim.set('doc', this);
+		if (!this.get('file') && !this.get('appFile')) {
+			throw 'No file';
+		}
+		var that = this;
+		if (this.get('appFile')) {
+			var appFile = this.get('appFile');
+			appFile.file(function(file) {
+				var reader = new FileReader();
+				reader.onload = function(e) {
+					log('reader.onload');
+					that.set('text', e.target.result);
+					that.setLines(that.get('text'));
+					vim.open(that);
+				};
+				reader.onerror = function(e) {
+					throw e;
+				};
+				log('reader.readAsText');
+				reader.readAsText(file);
+			}, function(e) {
+				throw e;
+			});
+		}
+
+		var reader = new FileReader();
+		reader.onload = function() {
+			log('reader.onload');
+			that.set('text', reader.result);
+			that.setLines(that.get('text'));
+			vim.open(that);
+		};
+		log('reader.readAsText');
+		reader.readAsText(this.get('file'));
+	},
+	save: function() {
+		log('doc.save');
+		if (this.get('file') || this.get('appFile')) {
+			this.saveFile();
+		}
+	},
+	saveFile: function() {
+		if (!this.get('file') && !this.get('appFile')) {
+			throw "No file";
+		}
+		var that = this;
+		if (this.get('file')) {
+			var file = this.get('file');
+			file.createWriter(function(writer) {
+				writer.onwriteend = function(e) {
+					that.trigger('saved');
+				};
+				writer.onerror = function(e) {
+					throw e;
+				}
+				writer.write(new Blob[that.toText()], {
+					type: 'text/plain'
+				});
+			}, function(e) {
+				console.log(e);
+			});
+		} else if (this.get('appFile')) {
+			var file = this.get('appFile');
+			file.createWriter(function(writer) {
+				log('file.createWriter');
+				writer.onerror = function(e) {
+					log('writer.onerror');
+					throw e;
+				};
+				var blob = new Blob([that.toText()], {
+					type: 'text/plain'
+				});
+				writer.truncate(blob.size);
+
+				//truncating is a write
+				writer.onwriteend = function() {
+					log('writer.truncate');
+
+					//then you actually write
+					writer.write(blob);
+					writer.onwriteend = function() {
+						log('writer.writeend');
+						that.trigger('ready');
+					}
+				}
+
+			}, function(e) {
+				throw e;
+			});
+		}
+
+	}
 });
 
 
 var DocV = Backbone.View.extend({
-    render: function() {
-        this.model.get('lines').forEach(function(line) {
-            var lineV = new LineV({model:line});
-            line.set('view',lineV);
-            $(this.el).append(lineV.render());
-        },this);
-    },
-    el: $(".doc"),
-    initialize: function() {
-        $(this.el).empty();
-        this.model.get('lines').on('add', function(line,col,options) {
-            var lineV = new LineV({model:line});
-            line.set('view',lineV);
-            
-            if(options && options.at) {
-                $(doc.get('lines').at(options.at-1).get('view').el).after(lineV.el);
-            } else {
-                $(this.el).append(lineV.el);
-            }
-            lineV.render();
-        },this);
-        this.render();
-    }
+	render: function() {
+		this.model.get('lines').forEach(function(line) {
+			var lineV = new LineV({
+				model: line
+			});
+			line.set('view', lineV);
+			$(this.el).append(lineV.render());
+		}, this);
+	},
+	el: $(".doc"),
+	initialize: function() {
+		$(this.el).empty();
+		this.model.get('lines').on('add', function(line, col, options) {
+			var lineV = new LineV({
+				model: line
+			});
+			line.set('view', lineV);
+
+			if (options && options.at) {
+				$(doc.get('lines').at(options.at - 1).get('view').el).after(lineV.el);
+			} else {
+				$(this.el).append(lineV.el);
+			}
+			lineV.render();
+		}, this);
+		this.render();
+	}
 });
