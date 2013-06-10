2dd1ead9d92d1ccb6d275d2ff5d1027b190485fb
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-10 13:20:11 -0400

    beginning test cases for learn vim progressively

---
+++ test/progressive.js
@@ -0,0 +1,46 @@
+//Test to ensure that learn vim progressively is an effective tutorial for this implementation:
+//http://yannesposito.com/Scratch/en/blog/Learn-Vim-Progressively/
+
+var vim = require('../index');
+
+var expect = function(assertion) {
+
+	return {
+		equal: function(obj) {
+			if(assertion == obj) return true;
+			throw "expected " + assertion + " to equal " + obj;
+		}
+	}
+};
+
+describe('survive', function(){
+
+	it('i enters insert mode', function() {
+			vim.exec('i');
+			expect(vim.modeName).equal('insert');
+	});
+
+	it('x deletes current character', function() {
+		vim.exec('esc');
+		vim.exec('i');
+		vim.exec('h');
+		expect(vim.curDoc.text().indexOf('h')).equal(0);
+		vim.exec('esc');
+		vim.exec('x');
+		expect(vim.curDoc.text().indexOf('h')).equal(-1);
+	});
+
+	//wq
+
+	it('dd delete and copy current line', function() {
+		vim.new();
+		vim.exec('i');
+		vim.exec('hello');
+		vim.exec('\n');
+		vim.exec('there');
+		vim.exec('esc');
+		vim.exec('dd');
+		//expect(vim.text()).equal('hello');
+		//console.log(vim.text());
+	});
+});
