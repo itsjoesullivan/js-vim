348beaf1afb815ddcd2757d062fa8446c6d111dd
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-11 19:29:35 -0400

    hm

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ var vim = require('./lib/Vim');
 if(typeof window !== 'undefined') {
   window.vim = vim;
   var mousetrap = require('./lib/3rd/mousetrap.js');
-  console.log(mousetrap);
   mousetrap.addEvent(document,'keypress', function(e) {
   	var key = mousetrap.characterFromEvent(e);
+  	console.log(key);
   	vim.exec(key);
-  	
   })
   mousetrap.addEvent(document,'keydown', function(e) { 
   	var key = mousetrap.characterFromEvent(e)
-  	console.log(key);
   	if(key === 'enter') key = '\n';
   	if('esc,backspace,return,\n'.split(',').indexOf(key) !== -1) {
   		e.preventDefault();
