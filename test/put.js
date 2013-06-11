277883d715f216ea1c3f344c54b8e08752579140
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-11 15:32:52 -0400

    put

---
+++ test/put.js
@@ -0,0 +1,40 @@
+describe('paste', function() {
+
+	var vim = require('../index');
+
+	var expect = function(assertion) {
+
+	return {
+		equal: function(obj) {
+			if(assertion == obj) return true;
+			throw "expected " + assertion + " to equal " + obj;
+		}
+	}
+
+};
+
+	var doc;
+
+	beforeEach(function() {
+		doc = new Doc({text:'asdf\nzxcv\nqwer'});
+		vim.curDoc = doc;
+	})
+
+	describe('p', function() {
+		it('inserts the contents of the first register after the cursor', function() {
+			vim.register(0,'hello');
+			vim.exec('p');
+			var text = vim.text();
+			expect(text.indexOf('ahello')).equal(0);
+			//expect(reg).equal('asdf');
+		});
+	});
+	describe('P', function() {
+		it('inserts the contents of the first register at the cursor', function() {
+			vim.register(0,'hello');
+			vim.exec('P');
+			var text = vim.text();
+			expect(text.indexOf('hello')).equal(0);
+		});
+	});
+});
