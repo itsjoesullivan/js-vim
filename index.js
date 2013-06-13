b4d39629af1623369d5b55155e50b5681c62d256
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-13 15:22:36 -0400

    Erased comments."

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ if(typeof window !== 'undefined') {
   var mousetrap = require('./lib/3rd/mousetrap.js');
   mousetrap.addEvent(document,'keypress', function(e) {
   	var key = mousetrap.characterFromEvent(e);
-  	console.log(key);
   	vim.exec(key);
   })
   mousetrap.addEvent(document,'keydown', function(e) { 
@@ -19,40 +18,6 @@ if(typeof window !== 'undefined') {
   });
   var view = require('./lib/browserView.js')(vim);
 } else {
+	var view = require('./lib/terminalView.js')(vim);
   module.exports = vim;
 }
-
-/*var vim = {
-
-	init: function(obj) {
-		this.editor = require('./lib/Vim');
-		if(typeof window !== 'undefined') return this.initBrowser(obj);
-	},
-	initBrowser: function() {
-
-		//establish el
-		if(typeof obj === 'object') {
-			if('el' in obj) {
-				this.el = obj.el;
-			} else {
-				throw "no element defined"; //maybe unwanted--headless usage?
-			}
-		}
-
-		//Listen for keystrokes
-	
-	console.log(moustrap);
-	/*var keys = require('./lib/keys');
-		keys(function() {
-			vim.exec.apply(vim,arguments);
-		});*/
-	
-		//Create view
-		/*var view = require('./lib/browserView');
-		view.init(this.editor);
-
-	},
-	initNode: function() {},
-
-};
-*/
