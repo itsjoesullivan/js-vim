b4a00564df274c4fc890de5a4cdea2ab0b3551b0
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-15 00:50:28 -0400

    handle tabs

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 var Vim = require('./lib/Vim');
-	console.log('here',Vim);
 
 	
 	var vim = new Vim();	
-	console.log('vim is:',vim)
-
+if(typeof window !== 'undefined') {
 
 	//Keys
 	var mousetrap = require('./lib/3rd/mousetrap.js');
  	 mousetrap.addEvent(document,'keypress', function(e) {
  	 	var key = mousetrap.characterFromEvent(e);
+    if(key === 'tab') {
+      e.preventDefault();
+    }
   	vim.exec(key);
   })
   mousetrap.addEvent(document,'keydown', function(e) { 
   	var key = mousetrap.characterFromEvent(e)
+    console.log(key);
   	if(key === 'enter') key = '\n';
-  	if('esc,backspace,return,\n'.split(',').indexOf(key) !== -1) {
+    if(key === 'tab') {
+      console.log('got here');
+      e.preventDefault();
+      key = '\t';
+    } 
+  	if('esc,backspace,return,\n,\t'.split(',').indexOf(key) !== -1) {
   		e.preventDefault();
   		vim.exec(key);
   	} 
@@ -23,6 +30,7 @@ var Vim = require('./lib/Vim');
 
 	//Instanciate view
   var view = require('./lib/browserView.js')(vim);
+}
 	if(typeof window === 'undefined') {
 		var view = require('./lib/terminalView.js')(vim);
 	}
