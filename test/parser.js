fd35a44d72eb86eeef6b6885d302a8b4bf3f4752
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-14 20:55:05 -0400

    added parser. Serious changes to vim.exec

---
+++ test/parser.js
@@ -0,0 +1,39 @@
+describe('parser', function() {
+
+	var vim = require('../index');
+
+	var expect = function(assertion) {
+
+	return {
+		equal: function(obj) {
+			if(assertion == obj) return true;
+			throw "expected " + assertion + " to equal " + obj;
+		}
+	}
+
+};
+
+	var doc;
+
+	beforeEach(function() {
+		doc = new Doc({text:'asdf aloha what\nzxcv\nqwer'});
+		vim.curDoc = doc;
+	});
+
+	describe('{count}{motion}', function() {
+		it('performs simple {motion} {count} times', function() {
+			var currentChar = vim.curDoc.cursor.char();
+			vim.exec('2l');
+			var newChar = vim.curDoc.cursor.char();
+			expect(newChar).equal(2)
+		});
+		it('performs search {motion} {count} times', function() {
+			var currentChar = vim.curDoc.cursor.char();
+			vim.exec('2/a\n');
+			var newChar = vim.curDoc.cursor.char();
+			expect(newChar).equal(9)
+		});
+
+	});
+
+});
