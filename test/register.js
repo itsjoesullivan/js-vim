4013a1eefdcc431dfa146fcb10afc41ad33afaf3
itsjoesullivan <itsjoesullivan@gmail.com>
2013-07-02 15:29:05 -0400

    closes #9 refs #10, #8, #7, #6, #5

--- test/register.js
+++ test/register.js
@@ -1,7 +1,7 @@
 describe('registers', function() {
 
 	var Vim = require('../index');
-	var vim = new Vim();
+	var vim
 
 	var expect = function(assertion) {
 
@@ -15,8 +15,8 @@ describe('registers', function() {
 
 	var doc;
 	beforeEach(function() {
-		doc = vim.new({text: 'hello\nis it me\nyou\'re looking for?'});
-		vim.curDoc = doc;
+		vim = new Vim();
+		vim.text( 'hello\nis it me\nyou\'re looking for?' )
 	});
 
 	describe('vim.register', function() {
@@ -29,40 +29,72 @@ describe('registers', function() {
 			expect(vim.register('a')).equal('hi');
 		});
 
-		it('returns an empty string for an empty buffer', function() {
+		it('returns an empty string for an empty register', function() {
 			expect(vim.register('b')).equal('');
 		});
 		
-		it('returns the buffer for a nonempty buffer', function() {
+		it('returns the register for a nonempty register', function() {
 			//vim.exec('yy');
 			vim.register(0,'hello\n');
 			expect(vim.register(0)).equal('hello\n');
 		});
-
-	});
-
-	/*describe('yy', function() {
-		it('places the current line into the first buffer', function() {
-			vim.exec('yy');
-			expect(vim.buffer(1)).equal('hello\n');
+		describe('%', function() {
+			it('returns the filename', function() {
+				vim.curDoc.path = 'index.js';
+				var name = vim.register('%');	
+				name.should.equal('index.js');
+			});	
 		});
 	});
 
+	/*describe('yy', function() {
+		it('places the current line into the first register', function() {
+			vim.exec('yy');
+			expect(vim.register(1)).equal('hello\n');
+		});
+	});
+
+	*/
+
 	describe('"ayy', function() {
-		it('places the first line into the "a" buffer', function() {
-			vim.exec('"ayy');
-			expect(vim.buffer('a')).equal('hello\n');
+	beforeEach(function() {
+		vim = new Vim();
+		vim.text('hello\nis it me\nyou\'re looking for?');
+	});
+		it('places the first line into the "a" register', function() {
+			vim.exec('"');
+			vim.exec('a');
+			vim.exec('yy');
+			JSON.stringify(vim.register('a')).should.equal(JSON.stringify(['o','hello','esc','0']));
 		});
 	});
 
 	describe('"ap', function() {
-		it('puts the contents of buffer "a" after the cursor', function() {
-			vim.buffer('a','aloha');
-			vim.exec('"ap');
+		it('puts the contents of register "a" after the cursor', function() {
+			vim.register('a','aloha');
+			vim.exec('"');
+			vim.exec('a');
+			vim.exec('p');
 			var text = vim.text();
 			var line = text.substring(0,text.indexOf('\n'));
 			expect(line).equal('halohaello');
 		});
-	});*/
+	});
 
+	describe('"%', function() {
+		it('retrieves the filename', function() {
+			var vim = new Vim();
+			vim.curDoc.path = 'asdf';
+			vim.exec('"');
+			vim.exec('%');
+			vim.exec('p');
+			vim.text().should.equal('asdf');
+		});
+		it('returns "" if no path specified', function() {
+
+		});
+		it('returns "" if path is not a string', function() {
+
+		});
+	});
 });
