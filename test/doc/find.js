bd0fb0d1e9c42dc2e771ff9491b5f12fbd8d5b25
itsjoesullivan <itsjoesullivan@gmail.com>
2013-07-12 17:14:25 -0400

    doc.find tweaks, tests, vim.curWord, tests, closes #105

--- test/doc/find.js
+++ test/doc/find.js
@@ -38,4 +38,28 @@ describe('Doc.find', function() {
         pos.char.should.equal(1);
         pos.line.should.equal(0);
     });
+	it('accepts inclusive arg', function() {
+		doc.text('a');
+		doc.cursor.char(0);
+		var x = doc.find(/(\w)/g, { inclusive: true }) 
+		x.found.should.equal(true);
+	});
+	it('accepts $ in forward search', function() {
+		doc.text('asdf');
+		doc.cursor.char(2);
+		var x = doc.find(/(\w)$/g);
+		x.found.should.equal(true);
+	});
+	it('accepts \W in forward search', function() {
+		doc.text('asdf ');
+		doc.cursor.char(2);
+		var x = doc.find(/(\w)\W/g);
+		x.found.should.equal(true);
+		x.char.should.equal(3);
+	});
+	it('finds word end', function() {
+		doc.text('asdf');
+		doc.cursor.char(3);
+		var pos = doc.find(/(\w)(?:$|\W)/g, { inclusive: true } );
+	});
 });
